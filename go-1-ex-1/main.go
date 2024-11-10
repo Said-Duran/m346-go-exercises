e18552c25c73bc73dfc719a856a23c79438e0985
Patrick Bucher <patrick.bucher@mailbox.org>
2024-11-10 11:43:16 +0100

    README: links to all exercises

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	var firstName string = "Said"
+	lastName := "Duran"
+	var dayOfBirth byte = 2
+	monthOfBirth := 10
+	yearOfBirth := 2006
+	numberOfSiblings := 4
+	var heightInMeters float32 = 1.75
+	var zodiacSign = '\u264e'
 	// TODO: Declare and initialize the variables being used in the output!
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
