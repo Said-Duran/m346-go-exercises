a7516857e4e46b9a59c1ba1c4c72acc8ca86f114
Said Duran <Said.Duran@kauz.ch>
2024-12-05 16:25:59 +0100

    Teil 3 Aufgabe 2 (noch nicht fertig)

--- go-3-ex-2/main.go
+++ go-3-ex-2/main.go
@@ -28,6 +28,36 @@ func outputDateRange(zodiacSign rune) {
 	} else {
 		fmt.Println("")
 	}
+
+	switch zodiacSign {
+	case 12:
+		fmt.Println("21.03. - 20.04")
+	case 11:
+		fmt.Println("21.04. - 20.05")
+	case 10:
+		fmt.Println("21.05. - 20.06")
+	case 9:
+		fmt.Println("21.06. - 20.07")
+	case 8:
+		fmt.Println("21.07. - 20.08")
+	case 7:
+		fmt.Println("21.08. - 20.09")
+	case 6:
+		fmt.Println("21.09. - 20.10")
+	case 5:
+		fmt.Println("21.10. - 20.11")
+	case 4:
+		fmt.Println("21.11. - 20.12")
+	case 3:
+		fmt.Println("21.12. - 20.01")
+	case 2:
+		fmt.Println("21.01. - 20.02")
+	case 1:
+		fmt.Println("21.02. - 20.03")
+	default:
+		fmt.Println("idk")
+	}
+
 	// TODO: ...and consider a default case.
 }
 
