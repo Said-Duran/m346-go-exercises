4b36544f764a4b3cf66fa2d02c4cfab5d7b84370
Said Duran <Said.Duran@kauz.ch>
2024-12-15 18:37:22 +0100

    teil 4 aufgaben 2 und 3

--- go-4-ex-2/main.go
+++ go-4-ex-2/main.go
@@ -1,7 +1,18 @@
 package main
 
-// TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+import (
+	"fmt"
+	"math"
+	"bufio"
+	"os"
+)
 
+// TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+func computeHypothenuse(a float64, b float64) float64 {
+    c := math.Sqrt(math.Pow(a, 2) + math.Pow(b, 2))
+	return c
+}
 func main() {
 	// TODO: call the function computeHypotenuse
+	fmt.Println(computeHypothenuse(15,30))
 }
--- go-4-ex-3/main.go
+++ go-4-ex-3/main.go
@@ -1,7 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
 // TODO: implement the function computeQuadraticFormula
+func computeQuadraticFormula(a float64, b float64, c float64) float64 {
+     d := math.Pow(b, 2) - 4 * a*c 
+	 x := (-b +- math.Sqrt(math.Pow(b,2) - 4*a*c) / 2*a )
+	 return d
+	 return x
+}
 
 func main() {
 	// TODO: call the function computeQuadraticFormula
+	fmt.Println(computeQuadraticFormula(12, 18, 9))
 }
